1af8bc88c55921e55406df371424c9abbf8b34c9
Superstar <bijingrover12@gmail.com>
2026-07-13 16:05:35 +0530

    Fixed Minor Bug in Result

--- Two Sum_1/Two Sum_Brute_Force.py
+++ Two Sum_1/Two Sum_Brute_Force.py
@@ -6,7 +6,7 @@ class Solution:
         for i in range(0, len(nums)):
             for j in range(i+1, len(nums)):
                 if nums[i] + nums[j] == target:
-                    result.append([i, j])
+                    result.extend([i, j])
         return result
 
 # test it
