958a954fb97131b0df5b3e782cc5d82dc1bc2ce3
Superstar <bijingrover12@gmail.com>
2026-07-23 15:52:55 +0530

    Factors of a number Optimized

--- Factors_of_Number/Factors_of_Number_Optimized.py
+++ Factors_of_Number/Factors_of_Number_Optimized.py
@@ -3,11 +3,12 @@ class Solution:
         l=[]
         if num==0:
             return [0]
-        for i in range(1,num+1): #Searches for all natural numbers in range of 1 to num/2
+        for i in range(1,(num//2)+1): #Searches for numbers from 1 to n/2 because  no factors of n lie beyond n/2 except for n itself
             if num%i==0: #searches for divisors of num
                 l.append(i) #appends numbers to l for final result
+        l.append(num)
         return l
             
 # test it
 sol = Solution()
-print(sol.factorial(0))
+print(sol.factorial(100))
