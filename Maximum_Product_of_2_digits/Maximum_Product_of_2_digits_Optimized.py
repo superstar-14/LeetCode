04c05b1731a28a05830e6b5bb50387802a67e835
Superstar <bijingrover12@gmail.com>
2026-07-25 23:36:42 +0530

    Maximum Product of 2 digits Optimized solution

---
+++ Maximum_Product_of_2_digits/Maximum_Product_of_2_digits_Optimized.py
@@ -0,0 +1,24 @@
+class Solution:
+    def prod(self,n):
+        m1,m2=0,0
+        while n>0:
+            a=n%10
+            if a>m1:
+                m1,m2=a,m1
+            elif a>m2:
+                m2=a
+            n=n//10
+        return m1*m2           
+            
+# test it
+sol = Solution()
+print(sol.prod(1978))
+
+
+
+
+
+
+
+
+
