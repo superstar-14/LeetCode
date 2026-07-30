5625fe2bb1adf2ce6e2a04624fbfd22695df0793
Superstar <bijingrover12@gmail.com>
2026-07-30 18:51:04 +0530

    Palindrome Recursion Correction

--- Palindrome/Palindrome_Recursion.py
+++ Palindrome/Palindrome_Recursion.py
@@ -1,17 +1,14 @@
 class Solution:
     def pal(self,st,i,j):
-        if st[i]==st[j]:
-            i=i+1
-            j=j-1
-            if i+1<-j:
-                self.pal(st,i,j)
-            return "Palindrome"
-        else:
+        if st[i] != st[j]:
             return "Not Palindrome"
-                  
+        if i>=j:
+            return "Palindrome"
+        return self.pal(st,i+1,j-1)  
 # test it
 sol = Solution()
-print(sol.pal("malyalam",0,-1))
+print(sol.pal("malayalam",0,len("malayalam")-1))
+
 
 
 
