44e712c52f9075d46c32c08e819c1a30f4afacd8
Superstar <bijingrover12@gmail.com>
2026-08-04 21:43:45 +0530

    Insertion Sort

---
+++ Insertion_Sort/Insertion_Sort.py
@@ -0,0 +1,25 @@
+class Solution:
+    def insertion(self, l):
+        for i in range(1,len(l)):
+            j=i-1
+            key=l[i]
+            while j>=0 and l[j]>key:
+                l[j+1]=l[j]
+                j=j-1
+            l[j+1]=key
+        return l
+
+            
+                
+            
+
+       
+                
+           
+# test it
+sol = Solution()
+print(sol.insertion([9,8]))
+
+
+
+
